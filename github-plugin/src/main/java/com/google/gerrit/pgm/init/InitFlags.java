f6ade2e008fabe849f42dc8e9c7038214d3078df
Luca Milanesio <luca.milanesio@gmail.com>
2013-07-27 23:34:57 +0100

    Moving dependencies to child module.
    Instead of getting all dependencies at parent Maven module
    they are now defined at child level.
    This allows to only include the ones effectively needed
    and avoid duplications when using shaded jars.
    
    Adding as well a copy of the Gerrit InitStep interface
    as it is missing in the Gerrit API package.
    
    Change-Id: I8c8edbc05599d1e507d269baf5d95f32b99870e7

---
+++ github-plugin/src/main/java/com/google/gerrit/pgm/init/InitFlags.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.init;
+
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
+
+import java.io.IOException;
+import java.util.List;
+
+/** Global variables used by the 'init' command. */
+@Singleton
+public class InitFlags {
+  /** Recursively delete the site path if initialization fails. */
+  public boolean deleteOnFailure;
+
+  /** Run the daemon (and open the web UI in a browser) after initialization. */
+  public boolean autoStart;
+
+  public final FileBasedConfig cfg;
+  public final FileBasedConfig sec;
+  public final List<String> installPlugins;
+
+  @Inject
+  InitFlags(final SitePaths site,
+      final @InstallPlugins List<String> installPlugins) throws IOException,
+      ConfigInvalidException {
+    this.installPlugins = installPlugins;
+    cfg = new FileBasedConfig(site.gerrit_config, FS.DETECTED);
+    sec = new FileBasedConfig(site.secure_config, FS.DETECTED);
+
+    cfg.load();
+    sec.load();
+  }
+}
