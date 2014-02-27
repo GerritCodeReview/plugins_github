a6e2351c575f3e2a7ea8e9bcafcb3c9790afba33
Luca Milanesio <luca.milanesio@gmail.com>
2014-02-27 18:52:06 +0000

    Resilience against GerritOAuth and JSESSIONID cookies lost
    GitHub plugin uses two main cookies to cache OAuth
    information:
    - GerritOAuth to enrich HTTP requests with auth headers used by
      Gerrit HTTP authentication
    - JSESSIONID to get session objects with GitHub client objects
    
    When GerritOAuth or JSESSIONID expire, a transparent GitHub OAuth
    renegotiation is needed to recreate them through a new authentication
    challenge. OAuth scope is now stored in the GerritOAuth cookie in order
    to re-create the same scope-level previously own.
    
    NOTE: GerritOAuth is typically short lived to avoid
          security leak of the OAuth information. JSESSIONID could be lost
          because of workload balancing or failover.
    
    Change-Id: I874c4ae75f2b238dccb0ec8c164628c28f1b5f01

--- github-oauth/src/main/java/com/googlesource/gerrit/plugins/github/oauth/OAuthCookie.java
+++ github-oauth/src/main/java/com/googlesource/gerrit/plugins/github/oauth/OAuthCookie.java
@@ -13,7 +13,12 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.github.oauth;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import javax.servlet.http.Cookie;
+
+import com.googlesource.gerrit.plugins.github.oauth.OAuthProtocol.Scope;
 
 public class OAuthCookie extends Cookie {
   private static final long serialVersionUID = 2771690299147135167L;
@@ -22,16 +27,34 @@ public class OAuthCookie extends Cookie {
   public final String user;
   public final String email;
   public final String fullName;
+  public final SortedSet<Scope> scopes;
 
   public OAuthCookie(TokenCipher cipher, final String user, final String email,
-      final String fullName) throws OAuthTokenException {
-    super(OAUTH_COOKIE_NAME, cipher.encode(String.format("%s\n%s\n%s", user,
-        email, fullName)));
+      final String fullName, final SortedSet<Scope> scopes) throws OAuthTokenException {
+    super(OAUTH_COOKIE_NAME, cipher.encode(getClearTextCookie(user, email, fullName, scopes)));
     this.user = user;
     this.email = email;
     this.fullName = fullName;
+    this.scopes = scopes;
     setMaxAge((int) (TokenCipher.COOKIE_TIMEOUT/1000L));
     setHttpOnly(true);
+    setPath("/");
+  }
+
+  private static String getClearTextCookie(final String user,
+      final String email, final String fullName, final SortedSet<Scope> scopes) {
+    StringBuilder clearTextCookie = new StringBuilder();
+    clearTextCookie.append(user);
+    clearTextCookie.append("\n");
+    clearTextCookie.append(email);
+    clearTextCookie.append("\n");
+    clearTextCookie.append(fullName);
+
+    for (Scope scope : scopes) {
+      clearTextCookie.append("\n");
+      clearTextCookie.append(scope);
+    }
+    return clearTextCookie.toString();
   }
 
   public OAuthCookie(TokenCipher cipher, Cookie cookie)
@@ -42,5 +65,11 @@ public class OAuthCookie extends Cookie {
     user = clearText.length > 0 ? clearText[0]:null;
     email = clearText.length > 1 ? clearText[1]:null;
     fullName = clearText.length > 2 ? clearText[2]:null;
+
+    this.scopes = new TreeSet<Scope>();
+    for (int i = 3; i < clearText.length; i++) {
+      Scope scope = Enum.valueOf(Scope.class, clearText[i]);
+      this.scopes.add(scope);
+    }
   }
 }
