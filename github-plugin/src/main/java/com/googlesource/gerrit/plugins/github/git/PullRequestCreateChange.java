6393386c47c9babdc7ba336bea252792f753cf8e
Luca Milanesio <luca.milanesio@gmail.com>
2014-06-18 15:27:26 +0100

    Define current patch-set for new Changes
    When creating new Changes from a Pull Request commit,
    we need to set the currently active patch-set on
    the Gerrit Change record.
    
    Change-Id: Ic33def077d8cc6d6da3796892eda147de8dea020

--- github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/PullRequestCreateChange.java
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/PullRequestCreateChange.java
@@ -52,6 +52,8 @@ import com.google.gerrit.server.events.CommitReceivedEvent;
 import com.google.gerrit.server.git.MergeException;
 import com.google.gerrit.server.git.validators.CommitValidationException;
 import com.google.gerrit.server.git.validators.CommitValidators;
+import com.google.gerrit.server.patch.PatchSetInfoFactory;
+import com.google.gerrit.server.patch.PatchSetInfoNotAvailableException;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.project.InvalidChangeOperationException;
 import com.google.gerrit.server.project.NoSuchChangeException;
@@ -73,6 +75,7 @@ public class PullRequestCreateChange {
   private final CommitValidators.Factory commitValidatorsFactory;
   private final ChangeInserter.Factory changeInserterFactory;
   private final PatchSetInserter.Factory patchSetInserterFactory;
+  private final PatchSetInfoFactory patchSetInfoFactory;
   private final ProjectControl.Factory projectControlFactory;
   private final GenericFactory userFactory;
 
@@ -82,12 +85,14 @@ public class PullRequestCreateChange {
       final CommitValidators.Factory commitValidatorsFactory,
       final ChangeInserter.Factory changeInserterFactory,
       final PatchSetInserter.Factory patchSetInserterFactory,
+      final PatchSetInfoFactory patchSetInfoFactory,
       final ProjectControl.Factory projectControlFactory,
       final IdentifiedUser.GenericFactory userFactory) {
     this.currentUser = currentUser;
     this.commitValidatorsFactory = commitValidatorsFactory;
     this.changeInserterFactory = changeInserterFactory;
     this.patchSetInserterFactory = patchSetInserterFactory;
+    this.patchSetInfoFactory = patchSetInfoFactory;
     this.projectControlFactory = projectControlFactory;
     this.userFactory = userFactory;
   }
@@ -98,7 +103,8 @@ public class PullRequestCreateChange {
       final String pullRequestMesage, final String topic, boolean doValidation)
       throws NoSuchChangeException, EmailException, OrmException,
       MissingObjectException, IncorrectObjectTypeException, IOException,
-      InvalidChangeOperationException, MergeException, NoSuchProjectException {
+      InvalidChangeOperationException, MergeException, NoSuchProjectException,
+      PatchSetInfoNotAvailableException {
     Id newChange = null;
     if (destinationBranch == null || destinationBranch.length() == 0) {
       throw new InvalidChangeOperationException(
@@ -210,7 +216,7 @@ public class PullRequestCreateChange {
       Ref destRef, Account.Id pullRequestOwner, RevCommit pullRequestCommit,
       RefControl refControl, String pullRequestMessage, String topic,
       boolean doValidation) throws OrmException,
-      InvalidChangeOperationException, IOException {
+      InvalidChangeOperationException, IOException, PatchSetInfoNotAvailableException {
     Change change =
         new Change(changeKey, new Change.Id(db.nextChangeId()),
             pullRequestOwner, new Branch.NameKey(project, destRef.getName()),
@@ -236,6 +242,8 @@ public class PullRequestCreateChange {
           ru.getResult()));
     }
 
+    change.setCurrentPatchSet(
+        patchSetInfoFactory.get(change, newPatchSet));
     ins.setMessage(
         buildChangeMessage(db, change, newPatchSet, pullRequestOwner, pullRequestMessage))
         .insert();
