017001acfca1c967c73b9d6d2ec53be6a0452283
Luca Milanesio <luca.milanesio@gmail.com>
2014-07-16 17:52:30 +0100

    GitHub organisations plugged as GroupBackend
    Initial support for using GitHub organisations
    plugged into Gerrit as GroupBackend and thus visibles from:
    - User’s group membership (/#/settings/group-memberships)
    - Project’s access control (/#/admin/projects/All-Projects,access)
    
    By typing github/OrgName in the Project’s access screen you will see
    listed all the organisations that your user belongs to and you can then
    assign Gerrit project permissions to it.
    The members of that organisation will automatically be granted 
    permissions to the Gerrit project. 
    
    Membership is resolved at runtime by making a GitHub API call using
    the current user scope, with a cache TTL of 15’. This means
    that only 4 GitHub calls/h per user can be actually consumed 
    by group resolution. Changes on GitHub organisation membership
    will be then detected by Gerrit after at least 15’.
    
    Gerrit cache for GitHub organisations membership is called:
    github-plugin-organisations.
    
    Change-Id: I051cd44c978b7f25a5508a33c298fd18b904177e

--- github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/GuiceModule.java
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/GuiceModule.java
@@ -16,11 +16,32 @@ package com.googlesource.gerrit.plugins.github;
 
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.webui.TopMenu;
+import com.google.gerrit.server.account.GroupBackend;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.googlesource.gerrit.plugins.github.group.GitHubGroupBackend;
+import com.googlesource.gerrit.plugins.github.group.GitHubGroupMembership;
+import com.googlesource.gerrit.plugins.github.group.GitHubOrganisationsCache;
+import com.googlesource.gerrit.plugins.github.group.GitHubOrganisationGroup;
+import com.googlesource.gerrit.plugins.github.oauth.GitHubLogin;
+import com.googlesource.gerrit.plugins.github.oauth.IdentifiedUserGitHubLoginProvider;
+import com.googlesource.gerrit.plugins.github.oauth.UserScopedProvider;
 
 public class GuiceModule extends AbstractModule {
   @Override
   protected void configure() {
+    bind(new TypeLiteral<UserScopedProvider<GitHubLogin>>() {}).to(
+        IdentifiedUserGitHubLoginProvider.class);
+
+    install(GitHubOrganisationsCache.module());
+
     DynamicSet.bind(binder(), TopMenu.class).to(GitHubTopMenu.class);
+    DynamicSet.bind(binder(), GroupBackend.class).to(GitHubGroupBackend.class);
+
+    install(new FactoryModuleBuilder()
+        .build(GitHubOrganisationGroup.Factory.class));
+    install(new FactoryModuleBuilder()
+        .build(GitHubGroupMembership.Factory.class));
   }
 }
