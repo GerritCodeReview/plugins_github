ef41d4d1113db3e3f82fb753d3dbe39730433fe3
Yuki Yugui Sonoda <yugui@yugui.jp>
2015-07-20 19:05:24 +0900

    [WIP] Add webhook to automatically import pull requests
    Adds a new servlet which handles webhook events sent from Github.
    The servlet forwards the payload to an EventHandler based on the type of
    the event, then the handler does the actual work.
    
    Currently "ping" and "pull_request" events are supported
    
    TODO(yugui) Move the import operation into a background thread?
    TODO(yugui) Write a journal record before importing so that Gerrit does
    not lose the import operation even if it is shutdown during the import
    
    Change-Id: I389f78c702dee1c59192768acffcff49e0c80a62

---
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/notification/PingHandler.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.github.notification;
+
+import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kohsuke.github.GHEventPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.inject.Singleton;
+
+// Handles ping event in github webhook.
+// @see <a href="https://developer.github.com/webhooks/#ping-event">Ping
+// Event</a>
+@Singleton
+class PingHandler implements EventHandler<PingHandler.Ping> {
+  private static Logger logger = LoggerFactory.getLogger(PingHandler.class);
+
+  @JsonAutoDetect
+  static class Ping {
+    @JsonProperty("zen")
+    String zen;
+    @JsonProperty("hook_id")
+    int hookId;
+  }
+
+  @Override
+  public void doAction(Ping payload, HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    logger.info("ping id={} zen={}", payload.zen, payload.hookId);
+    resp.setStatus(SC_NO_CONTENT);
+  }
+
+  @Override
+  public Class<Ping> getPayloadType() {
+    return Ping.class;
+  }
+}
