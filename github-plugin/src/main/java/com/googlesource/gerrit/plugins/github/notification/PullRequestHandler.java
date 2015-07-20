505cd85c8b74f8373294a065afe1f71ab69c1655
Yuki Yugui Sonoda <yugui@yugui.jp>
2015-07-20 19:05:24 +0900

    Add webhook to automatically import pull requests
    Adds a new servlet which handles webhook events sent from Github.
    The servlet forwards the payload to an EventHandler based on the type of
    the event, then the handler does the actual work.
    
    Currently "ping" and "pull_request" events are supported
    
    Change-Id: I389f78c702dee1c59192768acffcff49e0c80a62

---
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/notification/PullRequestHandler.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.github.notification;
+
+import java.io.IOException;
+
+import org.kohsuke.github.GHEventPayload.PullRequest;
+import org.kohsuke.github.GHRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.github.git.PullRequestImportType;
+import com.googlesource.gerrit.plugins.github.git.PullRequestImporter;
+
+/**
+ * Handles pull_request event in github webhook.
+ *
+ * @see <a href=
+ *      "https://developer.github.com/v3/activity/events/types/#pullrequestevent">
+ *      Pull Request Event</a>
+ */
+@Singleton
+class PullRequestHandler implements WebhookEventHandler<PullRequest> {
+  private static final Logger logger = LoggerFactory
+      .getLogger(PullRequestHandler.class);
+  private final Provider<PullRequestImporter> prImportProvider;
+
+  @Inject
+  public PullRequestHandler(Provider<PullRequestImporter> pullRequestsImporter) {
+    this.prImportProvider = pullRequestsImporter;
+  }
+
+  @Override
+  public boolean doAction(PullRequest payload) throws IOException {
+    String action = payload.getAction();
+    if (action.equals("opened") || action.equals("synchronize")) {
+      GHRepository repository = payload.getRepository();
+      int prNumber = payload.getNumber();
+      PullRequestImporter prImporter = prImportProvider.get();
+      String organization = repository.getOwnerName();
+      String name = repository.getName();
+      logger.info("Importing {}/{}#{}", organization, name, prNumber);
+      prImporter.importPullRequest(0, organization, name, prNumber,
+          PullRequestImportType.Commits);
+      logger.info("Imported {}/{}#{}", organization, name, prNumber);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public Class<PullRequest> getPayloadType() {
+    return PullRequest.class;
+  }
+}
---
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/notification/WebhookEventHandler.java
@@ -0,0 +1,37 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.github.notification;
+
+import java.io.IOException;
+
+import org.kohsuke.github.GHEvent;
+
+/**
+ * Abstract interface to handler which is responsible for a specific github
+ * webhook event type.
+ *
+ * Implementation classes must be named by the convention which
+ * {@link WebhookServlet#getWebhookClassName(GHEvent)} defines.
+ *
+ * @param <T> Type of payload. Must be consistent to the event type.
+ *
+ * @return true if the event has been successfully processed
+ */
+interface WebhookEventHandler<T> {
+  Class<T> getPayloadType();
+
+  boolean doAction(T payload)
+      throws IOException;
+}
