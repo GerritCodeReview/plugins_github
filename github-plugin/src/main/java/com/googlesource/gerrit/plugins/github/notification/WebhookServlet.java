ee1a3df7cea2ed1b7d41b34d3a76b0e2e545d0ae
David Pursehouse <david.pursehouse@sonymobile.com>
2015-09-17 10:07:10 +0900

    Remove unused import in WebhookServlet
    Change-Id: I4c28a926530cede47d64ed1e50cedfcd6ff298ca

--- github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/notification/WebhookServlet.java
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/notification/WebhookServlet.java
@@ -26,7 +26,6 @@ import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
