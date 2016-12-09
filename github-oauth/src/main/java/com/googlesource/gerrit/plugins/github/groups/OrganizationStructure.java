988ca22647e9d7a8df99bba39a49223c1d75ae8c
Luca Milanesio <luca.milanesio@gmail.com>
2016-12-09 23:37:55 +0000

    Use persistent cache for GitHub groups
    Retrieval of GitHub groups is a very expensive operation
    and causes the start-up of Gerrit to be very sloppy.
    
    By using a persistent cache we should avoid to rescan all
    the GitHub groups when rescanning the users and changes.
    
    Change-Id: Ib1c67f17d45ca63c54f5c801859cced42b756b54

---
+++ github-oauth/src/main/java/com/googlesource/gerrit/plugins/github/groups/OrganizationStructure.java
@@ -0,0 +1,55 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.github.groups;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.common.base.MoreObjects;
+
+public class OrganizationStructure implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private HashMap<String, HashSet<String>> teams = new HashMap<>();
+
+  public Set<String> put(String organisation, String team) {
+    HashSet<String> userTeams =
+        MoreObjects.firstNonNull(teams.get(organisation),
+            new HashSet<String>());
+    userTeams.add(team);
+    return teams.put(organisation, userTeams);
+  }
+
+  public Set<String> keySet() {
+    return teams.keySet();
+  }
+
+  public Iterable<String> get(String organization) {
+    return teams.get(organization);
+  }
+
+  @Override
+  public String toString() {
+    return teams
+        .entrySet()
+        .stream()
+        .map(
+            org -> "Organization " + org.getKey() + " Teams: " + org.getValue())
+        .collect(Collectors.joining(" : "));
+  }
+}
