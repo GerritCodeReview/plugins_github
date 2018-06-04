011485991d1ad5ee36c537e72ccc7ed50e55b9ed
Luca Milanesio <luca.milanesio@gmail.com>
2018-06-04 23:46:11 +0100

    Import GitHub Teams as Gerrit Groups
    Give the ability to discover existing GitHub Teams
    and create the corresponding Gerrit Groups, linking the
    existing teams as default members of the Gerrit counterparts.
    
    Feature: Issue 9157
    Change-Id: I7c2e39b90848dbf7a8dcfd2fed0951e988c6e1ff

---
+++ github-plugin/src/main/resources/static/js/teams.js
@@ -0,0 +1,182 @@
+$(function() {
+	var refreshInterval;
+
+	var completed = false;
+	var running = false;
+
+	var refresh = function() {
+		$.post('teams-import-status.gh', function(data) {
+			var teams = eval('(' + data + ')');
+			var allCompleted = false;
+			if(teams.length == 0) {
+				$("#submit").prop("disabled", "disabled");
+			} else {
+				allCompleted = true;
+				for (var i=0; i<teams.length; i++) {
+					var id = teams[i].index;
+					var value = teams[i].value;
+					var status = teams[i].status.toLowerCase();
+					$("#status_" + id).attr("class", "status " + status);
+					$("#repo_" + id).text(value);
+					if(status == 'sync') {
+						allCompleted = false;
+					}
+				}
+			}
+			
+			if(allCompleted || teams.length <= 0) {
+				completed = true;
+				$("#submit").prop("disabled", "");
+				$("#submit").html("<span class=\"button green\"><span>Next &gt;</span></span>")
+				clearInterval(refreshInterval);
+				running = false;
+			}
+		});
+	}
+	
+	$("select#organisation").change(function() {
+		completed = false;
+		running = false;
+		loadTeams();
+		$("input#filter").val("");
+	});
+	
+	$("select#numitems").change(function() {
+		filterTeams();
+	})
+	
+	var filterTimeout;
+	$("#filter").keyup(function() {
+		if (filterTimeout) {
+			clearTimeout(filterTimeout);
+		}
+		filterTimeout = setTimeout(function () {
+			filterTeams();
+		},500);
+	});
+	
+	var filterTeams = function() {
+		var filter = $("input#filter").val().toLowerCase();
+		var numTeams = 0;
+		$(".repo-sync li").each(function() {
+			var repoName = $(this).find("input.name").val();
+			var matched = repoName.toLowerCase().indexOf(filter)>=0;
+			var maxItems = $("select#numitems").val();
+			var checkbox = $(this).find("input.keycheckbox");
+			if(matched && numTeams < maxItems) {
+				$(this).attr("style","display: visible;");
+				$(this).find("input").removeAttr("disabled");
+				checkbox.prop("checked", true);
+				numTeams++;
+			} else {
+				$(this).attr("style","display: none;");
+				$(this).find("input").attr("disabled","disabled");
+				checkbox.prop("checked", false);
+			}
+		});
+	}
+	
+	$("#submit").click(function() {
+		var destination;
+
+		if(completed || $("ul.repo-sync li").length <= 0) {
+			$('#teams').submit();
+			return true;
+		} else {
+			$.ajax({
+				type : "POST",
+				url : "teams-import.gh",
+				data : $("#teams").serialize(),
+				success : function() {
+					$(".status").each(function() {
+						this.style.visibility = "visible";
+					});
+					$("ul.repo-sync .keycheckbox").each(function() {
+						if($(this).prop("checked")) {
+							$(this).prop("checked", false);
+							this.style.visibility = "hidden";
+						}
+					});
+					refresh();
+					refreshInterval = setInterval(refresh, 2000);
+				}
+			});
+			running = true;
+			$("#submit").prop("disabled", "disabled");
+			return false;
+		}
+	});
+	
+	$("#cancel").click(function() {
+		if(running) {
+			$.ajax({
+				type : "POST",
+				url : "teams-import-cancel.gh",
+				success: function() {
+					refresh();
+				}
+			});
+		} else {
+			window.location = "/";
+		}
+	});
+	
+	$("input#filter").focusin(function() {
+		$("input#filter").attr("placeholder","");
+	});
+	
+	$("input#selectallcheckbox").change(function() {
+		var checked = $("input#selectallcheckbox").prop("checked");
+		$("ul.repo-sync .keycheckbox").each(function() {
+			if(this.style.display != "none") {
+				$(this).prop("checked", checked);
+			}
+		});
+	});
+});
+
+var loadTeams = function () {
+	$("div.loading").attr("style","display: visible;");
+	$("ul.repo-sync").empty();
+	$("div.filter").attr("style","display: none;");
+	$("#submit").prop("disabled", "disabled");
+	
+	var organisation = $("select#organisation option:selected").val();
+	var filter = $("input#filter").val();
+	$.post('teams-list.gh',
+		{ "organisation": organisation, "filter": filter },
+		function(data) {
+		$("div.loading").attr("style","display: none;");
+		
+		var repos = eval('(' + data + ')');
+		var maxItems = $("select#numitems").val();
+		
+		for (var i=0; i<repos.length; i++) {
+			var repo = repos[i];
+			var repoLine = $(_.template($("#repo-sync-template").html(),
+					{ "index": i, 
+				      "repo": repo }));
+			if(i >= maxItems) {
+				repoLine.attr("style","display:none;");
+				repoLine.find("input").attr("disabled","disabled");
+				repoLine.find("input.keycheckbox").prop("checked", false);
+			} else {
+				repoLine.find("input.keycheckbox").prop("checked", true);
+			}
+			repoLine.appendTo("ul.repo-sync");
+		}
+		
+		if(repos.length > 0) {
+			$("#submit").html("<span class=\"button green\"><span>Import &gt;</span></span>")
+		} else {
+			$("#submit").html("<span class=\"button green\"><span>Next &gt;</span></span>")
+		}
+		$("#submit").prop("disabled", "");
+		$("div.filter").attr("style","display: visible;");
+	});
+};
+
+
+$(document).ready(function () {
+	loadTeams();
+});
