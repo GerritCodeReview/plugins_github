6126914e7c839d2dc8d2d99c13999f3850d028b6
Luca Milanesio <luca.milanesio@gmail.com>
2018-06-23 09:39:57 +0100

    Rename method to create new organisation directory
    Assign a proper name to the method responsible to create the new
    empty directory for the GitHub organisation, ready to host the clone
    of the GitHub Git repositories to import
    
    Change-Id: I3347d15c71d1712d21b1ebf12076e6c08e5820a6

--- github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/GitCloneStep.java
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/GitCloneStep.java
@@ -45,10 +45,10 @@ public class GitCloneStep extends ImportStep {
     super(gitConfig.gitHubUrl, organisation, repository, gitHubRepoFactory);
     LOG.debug("GitHub Clone " + organisation + "/" + repository);
     this.gitDir = gitConfig.gitDir.toFile();
-    this.destinationDirectory = getDestinationDirectory(organisation, repository);
+    this.destinationDirectory = prepareTargetGitDirectory(organisation, repository);
   }
 
-  private File getDestinationDirectory(String organisation, String repository)
+  private File prepareTargetGitDirectory(String organisation, String repository)
       throws GitDestinationAlreadyExistsException, GitDestinationNotWritableException {
     File orgDirectory = new File(gitDir, organisation);
     File destDirectory = new File(orgDirectory, repository + ".git");
@@ -88,10 +88,6 @@ public class GitCloneStep extends ImportStep {
 
   private boolean isNotEmpty(File destDirectory) {
     return destDirectory.listFiles().length > 0;
-  }
-
-  public File getDestinationDirectory() {
-    return destinationDirectory;
   }
 
   @Override
