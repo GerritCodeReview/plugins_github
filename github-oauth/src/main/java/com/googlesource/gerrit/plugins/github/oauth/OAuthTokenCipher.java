e4b5f7307752f83eaea8ef005d423ca549db661f
Antonio Barone <syntonyze@gmail.com>
2022-08-24 19:07:14 +0100

    Encrypt oauth token at rest
    The oauth token provided by Github is used together with the
    `github_oauth` schema, to generate a new external identity for the
    Gerrit user.
    
    The external-id is then persisted on disk as a note under the
    `refs/meta/external-ids` ref of the `All-Users.git` repo, as documented
    in [1].
    
    All credentials should be stored encrypted and protected at-rest, as the
    token could be used to impersonate the user associated to it.
    
    This vulnerability has been exploited in the past on other OAuth-based
    integrations [2].
    
    Avoid using plaintext oauth tokens for storing github external-ids by
    encrypting them first.
    
    [1] https://gerrit-documentation.storage.googleapis.com/Documentation/3.6.1/config-accounts.html#external-ids
    [2] https://github.blog/2022-04-15-security-alert-stolen-oauth-user-tokens/
    
    Bug: Issue 16192
    Change-Id: I69c2add9170159ab8b9a31098cf35f184e678401

---
+++ github-oauth/src/main/java/com/googlesource/gerrit/plugins/github/oauth/OAuthTokenCipher.java
@@ -0,0 +1,94 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.github.oauth;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+/** Provides the ability to encrypt and decrypt an OAuth token */
+@Singleton
+public class OAuthTokenCipher {
+  private final String cipherAlgorithm;
+  private final SecretKeySpec secretKey;
+
+  /**
+   * Constructs a {@code OAuthTokenCipher} using cipher algorithm specified in configuration
+   *
+   * @param config the github oauth configuration object
+   * @throws IOException when the cipher could not be constructed
+   */
+  @Inject
+  public OAuthTokenCipher(GitHubOAuthConfig config) throws IOException {
+    cipherAlgorithm = config.getCipherAlgorithm();
+    secretKey = new SecretKeySpec(config.readPassword(), config.getSecretKeyAlgorithm());
+  }
+
+  /**
+   * Encrypts the provided string and returns its base64 representation
+   *
+   * @param plainText the string to encrypt
+   * @return the base64-encoded encrypted string
+   * @throws CipherException when the string could not be encrypted
+   */
+  public String encrypt(final String plainText) throws CipherException {
+    try {
+      Cipher encryptCipher = initCipher(Cipher.ENCRYPT_MODE);
+      return Base64.getEncoder()
+          .encodeToString(encryptCipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8)));
+    } catch (IllegalBlockSizeException
+        | BadPaddingException
+        | NoSuchPaddingException
+        | NoSuchAlgorithmException
+        | InvalidKeyException e) {
+      throw new CipherException("Could not encrypt oauth token", e);
+    }
+  }
+
+  /**
+   * Decrypts the provided base64-encoded encrypted string
+   *
+   * @param base64EncryptedString the string to decrypt
+   * @return the plainText string
+   * @throws CipherException when the string could not be decrypted
+   */
+  public String decrypt(final String base64EncryptedString) throws CipherException {
+    try {
+      Cipher decryptCipher = initCipher(Cipher.DECRYPT_MODE);
+      return new String(decryptCipher.doFinal(Base64.getDecoder().decode(base64EncryptedString)));
+    } catch (IllegalBlockSizeException
+        | BadPaddingException
+        | NoSuchPaddingException
+        | NoSuchAlgorithmException
+        | InvalidKeyException e) {
+      throw new CipherException("Could not decrypt oauth token", e);
+    }
+  }
+
+  private Cipher initCipher(int mode)
+      throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    Cipher cipher = Cipher.getInstance(cipherAlgorithm);
+    cipher.init(mode, secretKey);
+    return cipher;
+  }
+}
