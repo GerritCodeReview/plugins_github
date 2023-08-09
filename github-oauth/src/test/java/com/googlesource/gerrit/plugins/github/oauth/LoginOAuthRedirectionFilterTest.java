47f653dd1240f2a380225c9ce4669378a499aa3c
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-08-09 10:25:51 +0100

    Allow login in a different domain
    A new login flow has been provided to allow requests from the domain i.e
    subdomain-2.domain-1.io to be redirected to a different domain i.e
    subdomain-1.domain-1.io in order to achieve the login. Once the the login
    has been done successfuly, the flow will be redirected to the initial
    domain subdomain-2.domain-1.io.
    
    TODO: explain the motivation of this change and add documentation in
    README.md
    
    Change-Id: I8e037f3c5580be2a63f517df769878e8b6e81921

---
+++ github-oauth/src/test/java/com/googlesource/gerrit/plugins/github/oauth/LoginOAuthRedirectionFilterTest.java
@@ -0,0 +1,86 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.github.oauth;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.net.HttpHeaders;
+import java.io.IOException;
+import java.net.URLEncoder;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginOAuthRedirectionFilterTest {
+  @Mock private HttpServletRequest httpServletRequest;
+  @Mock private HttpServletResponse httpServletResponse;
+  @Mock private FilterChain filterChain;
+
+  private final String sourceHost1 = "my-subdomain-1.my-domain.com";
+  private final String sourceHost2 = "my-subdomain-2.my-domain.com";
+  private final String sourceSSLPort = "443";
+  private final String sourceHttpsProtocol = "https";
+  private final String canonicalUrl = "https://my-subdomain-1.my-domain.com/";
+
+  @Test
+  public void shouldGoNextInChainWhenXForwardedHeadersNotPresent()
+      throws IOException, ServletException {
+    LoginOAuthRedirectionFilter loginOAuthRedirection =
+        new LoginOAuthRedirectionFilter(canonicalUrl);
+    loginOAuthRedirection.doFilter(httpServletRequest, httpServletResponse, filterChain);
+    verify(httpServletRequest).getHeader(HttpHeaders.X_FORWARDED_HOST);
+    verify(httpServletRequest).getHeader(HttpHeaders.X_FORWARDED_PORT);
+    verify(httpServletRequest).getHeader(HttpHeaders.X_FORWARDED_PROTO);
+    verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+  }
+
+  @Test
+  public void shouldRedirectToLoginWhenSourceDomainAndCanonicalWebDomainAreTheSame()
+      throws IOException, ServletException {
+    when(httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_HOST)).thenReturn(sourceHost1);
+    when(httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_PORT)).thenReturn(sourceSSLPort);
+    when(httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_PROTO))
+        .thenReturn(sourceHttpsProtocol);
+    LoginOAuthRedirectionFilter loginOAuthRedirection =
+        new LoginOAuthRedirectionFilter(canonicalUrl);
+    loginOAuthRedirection.doFilter(httpServletRequest, httpServletResponse, filterChain);
+    verify(httpServletResponse).sendRedirect(GitHubOAuthConfig.GERRIT_LOGIN);
+  }
+
+  @Test
+  public void
+      shouldRedirectToLoginWithFinalRedirectParamWhenSourceDomainAndCanonicalWebDomainAreNotTheSame()
+          throws IOException, ServletException {
+    when(httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_HOST)).thenReturn(sourceHost2);
+    when(httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_PORT)).thenReturn(sourceSSLPort);
+    when(httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_PROTO))
+        .thenReturn(sourceHttpsProtocol);
+    LoginOAuthRedirectionFilter loginOAuthRedirection =
+        new LoginOAuthRedirectionFilter(canonicalUrl);
+    loginOAuthRedirection.doFilter(httpServletRequest, httpServletResponse, filterChain);
+    String finalRedirectUrlEncoded =
+        URLEncoder.encode(
+            sourceHttpsProtocol + "://" + sourceHost2 + ":" + sourceSSLPort + "/", "UTF-8");
+    verify(httpServletResponse)
+        .sendRedirect(canonicalUrl + "login?final_redirect_url=" + finalRedirectUrlEncoded);
+  }
+}
