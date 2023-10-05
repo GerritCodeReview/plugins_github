09332178480f812cbfe2bc3514c287ce53ee2c90
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-10-05 18:30:47 +0100

    Report authentication progress to the user
    The GitHub OAuth API can get really slow, taking tens of seconds to
    respond. Which is fine when users do see that there's something going
    on in the backgournd.
    
    Unfortunately current implementation of scope selection and sing-in
    process gives no feedback about onging OAuth request. For some users
    this may feel like something is broken or Gerrit site is slow.
    
    To improve user experience we add progress "indicators" in both cases.
    
    In the scope selection, the "Login >" button will be disabled after
    clicking and its text will also change to 'Waiting for GitHub...'.
    
    In case of the 'Sign-in' link in Gerrit UI, we use the new 'auth-link'
    extension point to replace that link with custom component. The
    'gr-github-oauth-progress' will wait 550ms and then show a loading
    spinner (blocking the whole site) saying 'Waiting for GitHub...'.
    
    In both cases users are now informated that the authentication is taking
    place and they should wait for it to finish.
    
    Bug: Issue 302826306
    Change-Id: Ic4a8dc24d9133ac59dfe65858ac7ec8510cf4f09

---
+++ github-plugin/src/main/ts/gr-github-oauth-progress.ts
@@ -0,0 +1,72 @@
+import { PluginApi } from '@gerritcodereview/typescript-api/plugin';
+import { AuthInfo } from '@gerritcodereview/typescript-api/rest-api';
+import { CSSResult, LitElement, css, html } from "lit";
+import { customElement, property, query, state } from 'lit/decorators.js';
+
+@customElement('gr-github-oauth-progress')
+export class GrGitHubOAuthProgress extends LitElement {
+    @query('#gitHubOAuthProgress')
+    gitHubOAuthProgress?: HTMLDialogElement;
+
+    @property() plugin!: PluginApi;
+
+    @state() authInfo?: AuthInfo
+
+    @state() loggedIn?: boolean
+
+    override connectedCallback() {
+        super.connectedCallback();
+        const restApi = this.plugin.restApi();
+        if (!this.authInfo) {
+            restApi.getConfig().then(config => this.authInfo = config?.auth);
+        }
+        restApi.getLoggedIn().then(loggedIn => this.loggedIn = loggedIn);
+    }
+
+    static override get styles() {
+        return [
+            window.Gerrit.styles.spinner as CSSResult,
+            window.Gerrit.styles.font as CSSResult,
+            window.Gerrit.styles.modal as CSSResult,
+            css`
+            .loginButton {
+                --gr-button-text-color: var(--header-text-color);
+                color: var(--header-text-color);
+                padding: var(--spacing-m) var(--spacing-l);
+            }
+            .loadingContainer {
+                display: flex;
+                gap: var(--spacing-s);
+                align-items: baseline;
+                padding: var(--spacing-xxl);
+            }
+            .loadingSpin {
+                vertical-align: top;
+                position: relative;
+                top: 3px;
+            }
+          `];
+    }
+
+    override render() {
+        if (!this.authInfo || this.loggedIn !== false) {
+            return
+        }
+
+        return html`
+            <a class="loginButton" href=${this.authInfo.login_url} @click=${this.showModal}>
+                ${this.authInfo.login_text}
+            </a>
+            <dialog id="gitHubOAuthProgress">
+                <div class="loadingContainer">
+                    <span class="loadingSpin"></span>
+                    <span class="loadingText">Waiting for GitHub...</span>
+                </div>
+           </dialog>
+        `
+    }
+
+    private showModal() {
+        setTimeout(() => this.gitHubOAuthProgress?.showModal(), 550);
+    }
+}
