42b0d202d5e4653f0fbb80ad10b835cf06039e15
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-03-13 14:16:50 +0000

    Use replication plugin API to update and access its config
    Instead of directly modifying the replication plugin configuration files
    in the `$site_path/etc` directory use new API. This guarantee that the
    configuration will be stored and read in a correct format and location.
    
    Bug: Issue 325000746
    Depends-On: https://gerrit-review.googlesource.com/c/plugins/replication/+/412878
    Change-Id: I9bde7e1148d932fb6861f51edb3521449ec20bef

--- github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/ReplicateProjectStep.java
+++ github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/ReplicateProjectStep.java
@@ -16,19 +16,17 @@ package com.googlesource.gerrit.plugins.github.git;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import com.googlesource.gerrit.plugins.github.GitHubURL;
-import com.googlesource.gerrit.plugins.github.oauth.GitHubLogin;
-import com.googlesource.gerrit.plugins.github.oauth.ScopedProvider;
+import com.googlesource.gerrit.plugins.replication.ReplicationRemotesUpdater;
 import java.io.IOException;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReplicateProjectStep extends ImportStep {
   private static final Logger LOG = LoggerFactory.getLogger(ReplicateProjectStep.class);
-  private final ReplicationConfig replicationConfig;
-  private final String authUsername;
-  private final String authToken;
-  private final String gitHubUrl;
+  private final ReplicationRemotesUpdater replicationRemotesUpdater;
+  private final ReplicationRemoteConfigBuilder remoteConfigBuilder;
 
   public interface Factory {
     ReplicateProjectStep create(
@@ -37,20 +35,25 @@ public class ReplicateProjectStep extends ImportStep {
 
   @Inject
   public ReplicateProjectStep(
-      final ReplicationConfig replicationConfig,
+      final ReplicationRemotesUpdater replicationRemotesUpdater,
       final GitHubRepository.Factory gitHubRepoFactory,
-      final ScopedProvider<GitHubLogin> ghLoginProvider,
+      ReplicationRemoteConfigBuilder remoteConfigBuilder,
       @GitHubURL String gitHubUrl,
       @Assisted("organisation") String organisation,
       @Assisted("name") String repository)
       throws IOException {
     super(gitHubUrl, organisation, repository, gitHubRepoFactory);
+    this.remoteConfigBuilder = remoteConfigBuilder;
     LOG.debug("Gerrit ReplicateProject " + organisation + "/" + repository);
+<<<<<<< PATCH SET (7a9dba Use replication plugin API to update and access its config)
+    this.replicationRemotesUpdater = replicationRemotesUpdater;
+=======
     this.replicationConfig = replicationConfig;
     GitHubLogin ghLogin = ghLoginProvider.get();
     this.authUsername = ghLogin.getMyself().getLogin();
     this.authToken = ghLogin.getAccessToken();
     this.gitHubUrl = gitHubUrl;
+>>>>>>> BASE      (bf3124 Replace Lombok Getter with getX() methods on GitHubGroup)
   }
 
   @Override
@@ -58,11 +61,12 @@ public class ReplicateProjectStep extends ImportStep {
     progress.beginTask("Setting up Gerrit replication", 2);
 
     String repositoryName = getOrganisation() + "/" + getRepositoryName();
+    Config remoteConfig = remoteConfigBuilder.build(repositoryName);
     progress.update(1);
-    replicationConfig.addSecureCredentials(authUsername, authToken);
+
+    replicationRemotesUpdater.update(remoteConfig);
     progress.update(1);
-    replicationConfig.addReplicationRemote(
-        authUsername, gitHubUrl + "/${name}.git", repositoryName);
+
     progress.endTask();
   }
 
