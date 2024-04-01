91fde71cc5aa1f264409b1da5003698dac637c6b
David Ostrovsky <david@ostrovsky.org>
2024-04-01 15:08:30 +0200

    Add Bazel build
    This change adds Bazel build. Frontend build is not addressed in this
    change.
    
    One complication is the usage of lombok library, that has a known issue
    with bazel Turbine processor.  Add a workaround to disable running
    Turbine processor for related dependencies.
    
    The plugin is split in two different artifacts: github-oauth library
    and github-plugin.
    
    TEST PLAN:
    
    Clone the plugin into gerrit's plugins directory.
    
    Copy plugin's own external_plugin_deps.bzl into gerrit's plugins
    directory.
    
    To build the github-oauth library run:
    
      $> bazel build \
         plugins/github/github-oauth:github-oauth_deploy.jar
    
    To build the github-plugin run:
    
      $> bazel build plugins/github/github-plugin
    
    To run the tests:
    
      $> bazel test plugins/github/...
    
    Feature: Issue 10271
    Change-Id: I21e90d50f0a4db36a74a0c34123b3da6c0ec28da

---
+++ github-plugin/web/.eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+__plugindir = "github/github-plugin/web";
+module.exports = {
+  extends: "../../../.eslintrc.js",
+};
